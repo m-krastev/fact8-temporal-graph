d68b6208015dbb527317d6c7c1ac2decd35fa09f
MJHamar <58487170+MJHamar@users.noreply.github.com>
2024-01-27 22:57:16 +0100

    restored lambda param of UCT algorithm to 5 (#7)
    FEATURE: added AttnExplainerV2, as a new baseline that only considers the attention weights of the last layer in the target model for explanations. Cascaded through the call stack.
    
    * FEATURE: added AttnExplainerV2, as a new baseline that only considers the attention weights of the last layer in the target model for explanations. Cascaded through the call stack.
    
    DotProductNavigator implemented, needs testing

--- benchmarks/xgraph/run.sh
+++ benchmarks/xgraph/run.sh
@@ -19,6 +19,7 @@ do
 
         # baselines
         python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=attn_explainer_tg models=${model}
+        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=attn_explainer_v2 models=${model}
         python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pg_explainer_tg models=${model}
         python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pbone_explainer_tg models=${model}
     done
