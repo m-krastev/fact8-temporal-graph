a306fb303b5e9407862af9226c2130927cce6655
MJHamar <58487170+MJHamar@users.noreply.github.com>
2024-01-31 13:50:40 +0100

    Miki/restore original navigator (#19)
    * Restored original implementation in the new location of the navigator
    
    * FIXME: added th_num to the save path just to make sure no conflicts
    
    * minor fix and adapted run.sh
    
    * Added threshold_num to all save paths and introduced PGNavigator, which is their original navigator

--- benchmarks/xgraph/run.sh
+++ benchmarks/xgraph/run.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env zsh
 # run all explainers
 
-datasets=(simulate_v2 wikipedia reddit)
-models=(tgat tgn)
+datasets=(simulate_v1 simulate_v2 )
+models=(tgn tgat )
 
-cd $ROOT/benchmarks/xgraph
-processes=16
-
+cd "$ROOT/benchmarks/xgraph"
+echo "cwd: $PWD"
+processes=8
+threshold_num=$1
+echo "threshold num: ${threshold_num}"
 # alias python=/Users/Matey/project/fact8/.venv/bin/python3
 
 for dataset in ${datasets[@]}
@@ -16,12 +18,12 @@ do
     do
         echo "model: ${model}\n"
         # ours
-        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=subgraphx_tg models=${model} ++explainers.parallel_degree=$processes
+        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=subgraphx_tg models=${model} ++explainers.parallel_degree=$processes explainers.param.$dataset.threshold_num=$threshold_num
 
         # baselines
-        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=attn_explainer_tg models=${model} ++explainers.parallel_degree=$processes
-        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=attn_explainer_v2 models=${model} ++explainers.parallel_degree=$processes
-        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pg_explainer_tg models=${model} ++explainers.parallel_degree=$processes
-        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pbone_explainer_tg models=${model} ++explainers.parallel_degree=$processes
+        # python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=attn_explainer_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
+        # python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=attn_explainer_v2 models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
+        # python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pg_explainer_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
+        # python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pbone_explainer_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
     done
 done
