2b82e70f13c89585946e7d5b090d27a1248bdc90
mkrastev <matey_krastev2@hotmail.com>
2024-02-01 14:10:42 +0100

    cleanup and data added.

--- benchmarks/xgraph/run.sh
+++ benchmarks/xgraph/run.sh
@@ -7,9 +7,8 @@ models=(tgat tgn)
 cd "$ROOT/benchmarks/xgraph"
 echo "cwd: $PWD"
 processes=4
-threshold_num=$1
+threshold_num=$1 # NOTE: MUST BE SET
 echo "threshold num: ${threshold_num}"
-# alias python=/Users/Matey/project/fact8/.venv/bin/python3
 
 for dataset in ${datasets[@]}
 do
@@ -21,9 +20,9 @@ do
         python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=subgraphx_tg models=${model} ++explainers.parallel_degree=$processes explainers.param.$dataset.threshold_num=$threshold_num
 
         # baselines
-        # python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=attn_explainer_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
-        # python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=attn_explainer_v2 models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
-        # python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pg_explainer_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
-        # python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pbone_explainer_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
+        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=attn_explainer_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
+        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=attn_explainer_v2 models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
+        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pg_explainer_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
+        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pbone_explainer_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
     done
 done
