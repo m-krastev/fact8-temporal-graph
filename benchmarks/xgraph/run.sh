5d021cce7f7322c5b27ff8dfe1de4b1c128ac4af
mkrastev <matey_krastev2@hotmail.com>
2024-02-11 23:08:53 +0100

    Merge changes

--- benchmarks/xgraph/run.sh
+++ benchmarks/xgraph/run.sh
@@ -5,16 +5,9 @@ datasets=(simulate_v1 simulate_v2 reddit wikipedia)
 models=(tgat tgn)
 
 cd "$ROOT/benchmarks/xgraph"
-echo "cwd: $PWD"
-processes=1
-threshold_num=$1 # NOTE: MUST BE SET
 
-if [ -z "$threshold_num" ]
-then
-    echo "threshold_num is unset, set to number"
-    exit 1
-fi
-echo "threshold num: ${threshold_num}"
+# Number of parallel processes. Set to an appropriate value based on the available CPU cores.
+processes=1
 
 for dataset in ${datasets[@]}
 do
@@ -22,18 +15,27 @@ do
     for model in ${models[@]}
     do
         echo "model: ${model}\n"
-        # ours
-        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=subgraphx_tg models=${model} ++explainers.parallel_degree=$processes explainers.param.$dataset.threshold_num=$threshold_num ++explainers.navigator_type=mlp
-        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=subgraphx_tg models=${model} ++explainers.parallel_degree=$processes explainers.param.$dataset.threshold_num=$threshold_num ++explainers.navigator_type=pg
-        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=subgraphx_tg models=${model} ++explainers.parallel_degree=$processes explainers.param.$dataset.threshold_num=$threshold_num ++explainers.navigator_type=dot
-        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=subgraphx_tg models=${model} ++explainers.parallel_degree=$processes explainers.param.$dataset.threshold_num=$threshold_num ++explainers.use_navigator=False
+        # _____________________________________________
+        # _________ T-GNNExplainer variations _________
+        # _____________________________________________
 
+        # MLPNavigator
+        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=subgraphx_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.navigator_type=mlp
+        # PGNavigator
+        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=subgraphx_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.navigator_type=pg
+        # DotProductNavigator
+        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=subgraphx_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.navigator_type=dot
+        # No navigator
+        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=subgraphx_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.use_navigator=False
+        # _____________________________________________
+        # _________________ Baselines _________________
+        # _____________________________________________
 
-
-        # baselines
-        # python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=attn_explainer_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
-        # python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=attn_explainer_v2 models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
-        # python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pg_explainer_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
-        # python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pbone_explainer_tg models=${model} ++explainers.parallel_degree=$processes ++explainers.param.threshold_num=$threshold_num
+        # ATTN baseline
+        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=attn_explainer_tg models=${model} ++explainers.parallel_degree=$processes
+        # PGExplainer baseline
+        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pg_explainer_tg models=${model} ++explainers.parallel_degree=$processes
+        # PBONE baseline
+        python subgraphx_tg_run.py datasets=${dataset} device_id=0 explainers=pbone_explainer_tg models=${model} ++explainers.parallel_degree=$processes
     done
 done
